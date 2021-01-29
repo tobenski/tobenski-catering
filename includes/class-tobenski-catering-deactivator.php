d5a83e79c4dc890e6e11356a304188c171415032
Knud Rishøj <knud@det-gamle-posthus.dk>
2021-01-29 10:06:34 +0100

    first commit

---
+++ includes/class-tobenski-catering-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/tobenski/
+ * @since      1.0.0
+ *
+ * @package    Tobenski_Catering
+ * @subpackage Tobenski_Catering/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Tobenski_Catering
+ * @subpackage Tobenski_Catering/includes
+ * @author     Knud Rishøj <tirdyr@tobenski.dk>
+ */
+class Tobenski_Catering_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
